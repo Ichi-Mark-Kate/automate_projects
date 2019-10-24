e0af9f958934eab8dda0d9f18ea932e7e7bb3a2f
Katya Shapovalova <kshapovalova@kshapovalova-lx.nixsolutions.com>
2019-10-24 16:00:46 +0300

    add rename_image function to my_lb

--- my_library/my_lb.py
+++ my_library/my_lb.py
@@ -1,6 +1,32 @@
 import pyperclip
+import os, re
 
+def rename_image(path, file_format = "jpg", result_name = 'img'):
+    """Example: my_lb.rename_image(path = '/home/NIX/kshapovalova/automate_projects/test_rename_img_in_diff_folder');
+       - don't reuse this function on the same images. If you should then rename images: for new name not 
+       available image old name and result_name;
+    """
+    regexp = re.compile(r".*." + file_format)
+    i = 0
+    for folderName, subfolders, imgfilenames in sorted(os.walk(path)):
+        for img in sorted(imgfilenames):
+                mo = regexp.sub(result_name + str(i + 1) + "." + file_format, img)
+                os.rename(folderName + "/" + img, folderName + "/" + mo)
+                i += 1
+                if mo == None:
+                    continue
+
+"""
+The functions below are deprecated functions. Why:
+
+* comment() - can easily replace by multiline comment, which less time consuming;
+
+"""
 def comment(comand='drop'):
+    """Example: my_lb.comment(comand = 'add');
+       - copy text that you would like comment out and run this function;
+       - be aware when copying: first string must have all space symbols like in original text otherwise you to lose original format;
+    """
     text = pyperclip.paste()
     lines = text.split('\n')
 
@@ -13,3 +39,5 @@ def comment(comand='drop'):
     
     text = '\n'.join(lines)
     return print(text)
+    
+     
